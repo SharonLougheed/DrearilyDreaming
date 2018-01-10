9df514232d0cc97b521cee72c208f56ef41786d5
ACowboyKiller <walka.christopher@gmail.com>
2018-01-10 15:43:54 -0600

    Party Dream Dev Setup
    Imported standard assets for the standard Unity FPS Controller, created temp prefabs for party scene, built a mock-up for the party scene and wrote scripts to perform endless randomized world building.

---
+++ Assets/01_Scripts/01_Dev/02_Classes/CELL_MAP.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CELL_MAP
+{
+
+	#region --------------------	Public Fields
+
+	/// <summary>
+	/// The map cells in the scene.
+	/// </summary>
+	public static List<CELL> cellInstances = new List<CELL> ();
+
+	#endregion
+
+	#region --------------------	Public Methods
+
+	/// <summary>
+	/// Called when the player enters a new cell.  Moves the unused cells to the opposite side of the grid and regenerates them.
+	/// </summary>
+	/// <param name="cell_position">Cell position.</param>
+	public static void Enter_New_Cell ( Vector2 cell_position )
+	{
+		foreach ( CELL c in cellInstances )
+		{
+			if ( Mathf.Abs ( c.cellPosition.x - cell_position.x ) > 1 )
+			{
+				c.transform.position += new Vector3 ( ( c.cellPosition.x - cell_position.x > 0 ) ? -300.0f : 300.0f , 0, 0 );
+				c.Generate_Cell ();
+			}
+			if ( Mathf.Abs ( c.cellPosition.y - cell_position.y ) > 1 )
+			{
+				c.transform.position += new Vector3 ( 0, 0, ( c.cellPosition.y - cell_position.y > 0 ) ? -300.0f : 300.0f );
+				c.Generate_Cell ();
+			}
+		}
+	}
+
+	#endregion
+
+}
