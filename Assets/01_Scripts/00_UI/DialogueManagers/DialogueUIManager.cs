09895636956810b90d48eb8a2aeabbeaf000fcc1
dmp160030 <35274343+dmp1600301@users.noreply.github.com>
2018-01-29 01:09:33 -0600

    Notoriety and Floating Text
    Notoriety script written, but not in scene yet.
    Floating dialogue over npc's head now moves with npc and faces towards camera

--- Assets/01_Scripts/00_UI/DialogueManagers/DialogueUIManager.cs
+++ Assets/01_Scripts/00_UI/DialogueManagers/DialogueUIManager.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class DialogueUIManager : MonoBehaviour {
-    
+
     //text file with dialogue pre-written
     //public TextAsset dialogueFile;
-
+    private Transform mainCameraTransform;
     //child text ui object
     private Text dialogueTextChild;
+    private Transform parentTransform;
     
     private void Awake()
     {
         dialogueTextChild = GetComponentInChildren<Text>();
+        mainCameraTransform = GameObject.FindGameObjectWithTag("MainCamera").transform;
     }
     //private void Start()
     //{
     //    dialogueTextChild.text = dialogueFile.text;
     //}
+    private void Update() {
+        gameObject.transform.position = parentTransform.position + (Vector3.up * 1.5f);
+        gameObject.transform.LookAt(mainCameraTransform.position);
+    }
+    public void SetTransformToFollow(Transform t) {
+        parentTransform = t;
+    }
 }
--- Assets/01_Scripts/00_UI/DialogueManagers/PassiveDialogue.cs
+++ Assets/01_Scripts/00_UI/DialogueManagers/PassiveDialogue.cs
@@ -8,6 +8,10 @@ public class PassiveDialogue : MonoBehaviour {
     public GameObject dialogueUIPrefab;
     public float prefabDelayTime;
 
+    private Transform mainCameraTransform;
+    private void Awake() {
+        mainCameraTransform = GameObject.FindGameObjectWithTag("MainCamera").transform;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -16,9 +20,9 @@ public class PassiveDialogue : MonoBehaviour {
         }
     }
 
-    private IEnumerator PopupDialogue()
-    {
+    private IEnumerator PopupDialogue() {
         GameObject newDialouge = Instantiate(dialogueUIPrefab, transform.position + Vector3.up, Quaternion.identity);
+        newDialouge.GetComponent<DialogueUIManager>().SetTransformToFollow(gameObject.transform);
         yield return new WaitForSeconds(prefabDelayTime);
         Destroy(newDialouge);
     }
