932ebd88d2b0063f4ccedc1556906d2435a1d7c3
David Page <35274343+dmp1600301@users.noreply.github.com>
2018-02-02 17:40:30 -0600

    dialogue updates
    cleaned up dialogue table and pulled back the scope of the the current dialogue system.

--- Assets/01_Scripts/02_Classes/00_Generic/DialogueTable.cs
+++ Assets/01_Scripts/02_Classes/00_Generic/DialogueTable.cs
@@ -3,65 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DialogueTable : MonoBehaviour{
+    
+    //list of all dialogue files for this class
+    public TextAsset passiveTextFile,activeTextFile;
 
-    public string passiveAnswerKey,
-                  passiveQuestionKey,
-                  activeAnswerKey,
-                  activeQuestionKey;
-
-    public List<TextAsset> dreailyDreamingTextFiles;
-
-    public static Dictionary<string,string> passiveAnswerList,
-                                            passiveQuestionList,
-                                            activeAnswerList,
-                                            activeQuestionList;
+    [HideInInspector] public static List<string> passiveConversations,
+                                                 activeConversations;
+    
     private void Awake()
     {
         LoadTextAssets();
     }
     private void LoadTextAssets() {
-        //Loop through each file in the List
-        foreach(TextAsset textFile in dreailyDreamingTextFiles) 
+        string[] eachPassiveLine;
+        string passiveText = passiveTextFile.text;
+        eachPassiveLine = passiveText.Split('\n');
+
+        foreach(string line in eachPassiveLine)
         {
-            string[] eachLine;                  //array for storage of each full line of text from the file
-            string tempString = textFile.text;  //string variable to hold the data from the file
-                        
-            eachLine = tempString.Split('\n');  //parse data into individual lines
-            foreach (string section in eachLine)    //parse each line into two sections; index 0: dictionary key. index 1: string for storage
-            {
-                //formatting for the file should be "listkey_dialogue\n" ex. "passive answer_I am the real Slim Shady!"
-                string[] splitLine = section.Split('_');    //split the line at the underscore
-                string keyWord,dialogue;
-                //switch (textFile.name)
-                //{
-                //    case "passiveAnswer":
-                //        keyWord = splitLine[0];
-                //        dialogue = splitLine[1];
-                //        passiveAnswerList.Add(keyWord,dialogue);
-                //        break;
-                //    case "passiveQuestion":
-                //        keyWord = splitLine[0];
-                //        dialogue = splitLine[1];
-                //        passiveQuestionList.Add(keyWord, dialogue);
-                //        break;
-                //    case "activeAnswer":
-                //        keyWord = splitLine[0];
-                //        dialogue = splitLine[1];
-                //        activeAnswerList.Add(keyWord, dialogue);
-                //        break;
-                //    case "activeQuestion":
-                //        keyWord = splitLine[0];
-                //        dialogue = splitLine[1];
-                //        activeQuestionList.Add(keyWord, dialogue);
-                //        break;
-                //    default:
-                //        break;
-                //}
-            }
-            
+            passiveConversations.Add(line);
         }
 
-
-
+        /*
+        //string[] eachActiveLine;
+        //string activeText = activeTextFile.text;
+        //eachActiveLine = activeText.Split('\n');
+        
+        //string[] eachLine;                  //array for storage of each full line of text from the file
+        //string tempString = textFile.text;  //string variable to hold the data from the file
+                        
+        //eachLine = tempString.Split('\n');  //parse data into individual lines
+        //foreach (string section in eachLine)    //parse each line into two sections; index 0: dictionary key. index 1: string for storage
+        //{
+        //    //formatting for the file should be "listkey_dialogue\n" ex. "passive answer_I am the real Slim Shady!"
+        //    string[] splitLine = section.Split('_');    //split the line at the underscore
+        //    string keyWord,dialogue;
+        //    //switch (textFile.name)
+        //    //{
+        //    //    case "passiveAnswer":
+        //    //        keyWord = splitLine[0];
+        //    //        dialogue = splitLine[1];
+        //    //        passiveAnswerList.Add(keyWord,dialogue);
+        //    //        break;
+        //    //    case "passiveQuestion":
+        //    //        keyWord = splitLine[0];
+        //    //        dialogue = splitLine[1];
+        //    //        passiveQuestionList.Add(keyWord, dialogue);
+        //    //        break;
+        //    //    case "activeAnswer":
+        //    //        keyWord = splitLine[0];
+        //    //        dialogue = splitLine[1];
+        //    //        activeAnswerList.Add(keyWord, dialogue);
+        //    //        break;
+        //    //    case "activeQuestion":
+        //    //        keyWord = splitLine[0];
+        //    //        dialogue = splitLine[1];
+        //    //        activeQuestionList.Add(keyWord, dialogue);
+        //    //        break;
+        //    //    default:
+        //    //        break;
+        //    //}            
+        //}*/
     }
 }
