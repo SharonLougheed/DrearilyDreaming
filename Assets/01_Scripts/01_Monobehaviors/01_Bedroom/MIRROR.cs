e130f38976851ad37cec492fd5c3afdf9a25c2f2
ACowboyKiller <walka.christopher@gmail.com>
2018-02-19 23:16:14 -0600

    Small Modifications and Level Fixes
    Fixed the bedroom mirror to be reflective.  Stretched the 3rd person character to be a more realistic height.  Disabled the water script to fix the project lag.

---
+++ Assets/01_Scripts/01_Monobehaviors/01_Bedroom/MIRROR.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MIRROR : MonoBehaviour {
+
+	public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		Matrix4x4 mat = cam.projectionMatrix;
+		mat *= Matrix4x4.Scale(new Vector3(1, -1, 1));
+		cam.projectionMatrix = mat;
+	}
+}
