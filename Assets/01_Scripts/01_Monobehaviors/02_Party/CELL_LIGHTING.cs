13f7f87c8c0f88bed7e7b00f5f827d0b24a7ec84
dmp160030 <35274343+dmp1600301@users.noreply.github.com>
2018-02-26 02:54:12 -0600

    Light changes
    -changed script so the rate at which the light color changes can be changed in the editor.

--- Assets/01_Scripts/01_Monobehaviors/02_Party/CELL_LIGHTING.cs
+++ Assets/01_Scripts/01_Monobehaviors/02_Party/CELL_LIGHTING.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 [ RequireComponent ( typeof ( Light ) ) ]
 public class CELL_LIGHTING : MonoBehaviour 
 {
+    #region --------------------    Public Fields
+    /// <summary
+    /// upper and lower bounds
+    /// </summary>
+    public float goodRange, badRange;
+    #endregion  
+    #region --------------------	Private Fields
 
-	#region --------------------	Private Fields
-
-	/// <summary>
-	/// The light.
-	/// </summary>
-	private Light _light;
+    /// <summary>
+    /// The light.
+    /// </summary>
+    private Light _light;
 
 	#endregion
 
@@ -31,8 +36,8 @@ public class CELL_LIGHTING : MonoBehaviour
 	private void Update ()
 	{
 		_light.color = Color.Lerp ( _light.color, new Color ( 1.0f - Mathf.Clamp ( PlayerNotoriety.GetPlayerNotoriety () / 5.0f, 0, 1.0f ),
-			Mathf.Clamp ( 1.0f - Mathf.Abs ( PlayerNotoriety.GetPlayerNotoriety () / 5.0f ), 0, 1.0f ),
-			1.0f - Mathf.Clamp ( PlayerNotoriety.GetPlayerNotoriety () / -5.0f, 0, 1.0f ) ), Time.deltaTime );
+			Mathf.Clamp ( 1.0f - Mathf.Abs ( PlayerNotoriety.GetPlayerNotoriety () / goodRange), 0, 1.0f ),
+			1.0f - Mathf.Clamp ( PlayerNotoriety.GetPlayerNotoriety () / badRange, 0, 1.0f ) ), Time.deltaTime );
 	}
 
 	#endregion
