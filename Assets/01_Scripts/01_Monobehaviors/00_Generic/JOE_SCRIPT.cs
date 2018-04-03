3d90c6f7e1501abbcb3e75341258198294ddfa47
ACowboyKiller <walka.christopher@gmail.com>
2018-04-03 14:58:12 -0500

    Joe Spawns in random locations
    now

---
+++ Assets/01_Scripts/01_Monobehaviors/00_Generic/JOE_SCRIPT.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JOE_SCRIPT : MonoBehaviour 
+{
+
+	private NPC _npc;
+	[SerializeField] private List <Transform> _spawns;
+
+	// Use this for initialization
+	void Start () {
+		_npc = GetComponent <NPC> ();
+		_npc.agent.Warp ( _spawns [ Random.Range ( 0, _spawns.Count ) ].position );
+	}
+
+}
