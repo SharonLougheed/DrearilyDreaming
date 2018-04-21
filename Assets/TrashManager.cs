d62c9a7f298dd668b2790edc298499ddfc04b069
dmp160030 <35274343+dmp1600301@users.noreply.github.com>
2018-04-21 04:49:14 -0500

    Where's Rick Update
    -Rick likes to move around the room. Who knew he was such a sour explorer.
    -Trash shows up based on end of party state
    -added storyboards to ui in bedroom scene

---
+++ Assets/TrashManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashManager : MonoBehaviour {
+
+    public List<GameObject> _trash = new List<GameObject>();
+    
+    [SerializeField] private END_PARTY_STATE currentGameState;
+
+    private void Awake() {
+
+        GameManager _gm = GameManager.instance;
+
+        currentGameState = _gm.data.endPartyState;
+    }
+
+    // Use this for initialization
+    private void Start () {
+        Debug.Log("End Game State:\t" + currentGameState.ToString());
+        switch (currentGameState)
+        {
+            case END_PARTY_STATE.DEFAULT:
+                foreach(GameObject t in _trash)
+                {
+                    t.SetActive(false);
+                }
+                break;
+            case END_PARTY_STATE.WIN:
+                foreach (GameObject t in _trash)
+                {
+                    t.SetActive(false);
+                }
+                break;
+            case END_PARTY_STATE.LOSE:
+                foreach (GameObject t in _trash)
+                {
+                    t.SetActive(true);
+                }
+                break;
+        }
+	}
+}
---
+++ Assets/WheresRicksPickle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheresRicksPickle : MonoBehaviour {
+    public GameObject thePickle;
+    public Transform[] pickleSpawns;
+
+	// Use this for initialization
+	void Start () {
+        int i = (int)Random.Range(0f, pickleSpawns.Length);
+        thePickle.transform.SetPositionAndRotation(pickleSpawns[i].position,pickleSpawns[i].rotation);
+	}
+}
