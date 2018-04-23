d1a65d1c326a5d1a7fb52e986e5c47522580b2e0
dmp160030 <35274343+dmp1600301@users.noreply.github.com>
2018-04-23 17:00:53 -0500

    Bedroom bug fix
    fixed bu in befrrom making pause menu unclickable

--- Assets/01_Scripts/00_UI/UtilityManagers/StoryBoardManager.cs
+++ Assets/01_Scripts/00_UI/UtilityManagers/StoryBoardManager.cs
@@ -2,27 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeInState : MonoBehaviour {
+public class StoryBoardManager : MonoBehaviour {
 
-    public GameObject objectToTransform;
-    public Transform winState;
-    public Transform loseState;
-    
+    public GameObject goodEnding, badEnding;
+
     [SerializeField] private END_PARTY_STATE objectState;
 
     private void Awake() {
         objectState = GameManager.instance.data.endPartyState;
     }
+
     // Use this for initialization
     private void Start () {
         switch (objectState)
         {
+            case END_PARTY_STATE.DEFAULT:
+                goodEnding.SetActive(false);
+                badEnding.SetActive(false);
+                break;
             case END_PARTY_STATE.WIN:
-                objectToTransform.transform.SetPositionAndRotation(winState.position, winState.rotation);
+                goodEnding.SetActive(true);
                 break;
             case END_PARTY_STATE.LOSE:
-                objectToTransform.transform.SetPositionAndRotation(loseState.position, loseState.rotation);
+                goodEnding.SetActive(true);
                 break;
         }
-    }
+	}
 }
